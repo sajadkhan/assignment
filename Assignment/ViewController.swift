e3cff60a75ffb1bfdac14e2a8c5294d85880600b
Muhammad Sajad <muhammadsajad@muhammads-macbook-pro-2.local>
2019-02-17 20:15:28 +0500

    Initial Commit

---
+++ Assignment/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Assignment
+//
+//  Created by Muhammad Sajad on 17/02/2019.
+//  Copyright © 2019 Muhammad Sajad. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
